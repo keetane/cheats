95792fcb86f50e3a26dcfb50281f6941d9366c3e
keetane <yu.takahashi.k@gmail.com>
2025-04-26 09:31:36 +0900

    chain split was added

--- docking/redock.sh
+++ docking/redock.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 # sh redock.sh --pdb xxxx
 
-# 日付を取得
-today=$(date '+%Y%m%d')
+#!/bin/bash
 
-# 引数の処理
-pdb=""
-
-# オプションの解析
-while [ $# -gt 0 ]; do
-  case $1 in
-    --pdb)
-      pdb=$2
-      shift 2
-      ;;
-    *)
-      echo "Usage: $0 [--pdb pdb]"
-      exit 1
-      ;;
-  esac
-done
-
-# 引数が指定されていない場合の処理
-if [ -z "$pdb" ]; then
-  echo "Usage: $0 [--pdb pdb]"
-  exit 1
+# 使用法チェック
+if [ "$1" != "--pdb" ] || [ -z "$2" ]; then
+    echo "Usage: $0 --pdb <pdb_id>"
+    exit 1
 fi
 
-# ディレクトリ作成および移動
-mkdir -p "$pdb"
-cd "$pdb" || exit
+pdb=$(echo "$2" | tr '[:upper:]' '[:lower:]')  # 小文字に変換
+pdb_file="${pdb}.pdb"
+workdir="${pdb}_prep"
 
-# pdbfixerコマンドの実行
-pdbfixer --pdbid="$pdb" --output="prep_${pdb}_${today}.pdb" --ph=7
+# 1. 作業ディレクトリ作成＆移動
+mkdir -p "$workdir"
+cd "$workdir" || exit 1
 
-# receptorとligandの準備
-grep ATOM prep_${pdb}_${today}.pdb > rec_${pdb}.pdb
-grep HETATM prep_${pdb}_${today}.pdb > lig_${pdb}.pdb
+# 2. PDBファイルのダウンロード
+echo "Downloading PDB file for $pdb..."
+wget -q "https://files.rcsb.org/download/${pdb}.pdb" -O "$pdb_file"
+
+if [ ! -f "$pdb_file" ]; then
+    echo "Failed to download PDB file."
+    exit 1
+fi
+
+# 3. 全ATOM（=タンパク質）を1つのファイルにまとめる（チェーン分割しない）
+echo "Extracting full receptor (all chains)..."
+grep "^ATOM" "$pdb_file" > "rec_${pdb}.pdb"
+
+# 3. チェーンごとにタンパク質を分割
+echo "Splitting chains..."
+for chain in $(grep "^ATOM" "$pdb_file" | awk '{print substr($0, 22, 1)}' | sort | uniq); do
+    grep "^ATOM" "$pdb_file" | grep " $chain " > "${pdb}_${chain}.pdb"
+done
+
+# 4. chain A の水 (HOH) を抽出
+echo "Extracting water molecules from chain A..."
+grep "^HETATM" "$pdb_file" |grep "HOH" > solv_${pdb}.pdb
+
+# 5. 金属イオンを抽出
+echo "Extracting metals..."
+grep '^HETATM' "$pdb_file" | grep -E ' ZN | FE | MG | CA | NA | MN | CU | CO | K  ' > metal_${pdb}.pdb
+
+# 6. chain A の HETATM から水と金属を除いたリガンドを抽出
+echo "Extracting ligand from chain A (excluding water and metals)..."
+grep "^HETATM" "$pdb_file" | grep " A " | grep -v "HOH" | grep -v -E ' ZN | FE | MG | CA | NA | MN | CU | CO | K  ' > lig_${pdb}.pdb
 
 # local docking
 smina \
